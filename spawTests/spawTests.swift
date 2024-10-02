551a16c27be9865b2222a7e3126caa8e1a9f0654
loo-y <loo-y@outlook.com>
2024-10-02 10:48:20 +0800

    Initial Commit

---
+++ spawTests/spawTests.swift
@@ -0,0 +1,17 @@
+//
+//  spawTests.swift
+//  spawTests
+//
+//  Created by Loo on 2024/10/2.
+//
+
+import Testing
+@testable import spaw
+
+struct spawTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
